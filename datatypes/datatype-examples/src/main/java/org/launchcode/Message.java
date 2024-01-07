6cae15019ff696954489f94ef0e751d1d9d4492d
sungheechon <sungheechon@gmail.com>
2024-01-07 10:11:22 -0500

    practice files

--- datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
+++ datatypes/datatype-examples/src/main/java/org/launchcode/Message.java
@@ -8,6 +8,8 @@ public class Message {
             return "¡Hola, Mundo!";
         } else if (lang.equals("fr")) {
             return "Bonjour, le monde!";
+        } else if (lang.equals("kr")) {
+            return "Ahn-nyeong!";
         } else {
             return "Hello, World!";
         }
