877e373f1ddcc4cff154a5cbb654840fcdbad7b8
sungheechon <sungheechon@gmail.com>
2024-01-18 11:11:20 -0500

    UNIT 2 Java - CH 8: Interfaces and Polymorphism - Exercises

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +11,10 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
+        flavors.sort(new FlavorComparator());
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        cones.sort(new ConeComparator());
 
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
     }
